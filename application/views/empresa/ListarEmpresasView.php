b5bb6ddb4265e1091a5078fae0f208d56d56a1b7
Fernanda <nandinhaflss@gmail.com>
2013-03-28 18:02:59 -0300

    Configuração da Página Listar Empresas

--- application/views/empresa/ListarEmpresasView.php
+++ application/views/empresa/ListarEmpresasView.php
@@ -1,23 +1,37 @@
 
 {% include '/partials/Header.php' %}
 
-    <table cellpadding="0" cellspacing="0" border="0">
+<div class="wrap">
+    <h3>Empresas</h3>
+
+    <table cellpadding="0" cellspacing="0" border="0" class="table_lista">
 
        <!-- Cabeçalho da tabela -->
         <thead>
             <tr>
                 <th>Nome da Empresa</th>
+                <th>URL da empresa</th>
+                <th>Servidor de Entrada</th>
+                <th>Porta do Servidor de Entrada</th>
+                <th>Servidor de Saída</th>
+                <th>Porta do Servidor de Saída</th>
             </tr>
         </thead>
 
-        <tbody>
+        <tbody >
             {% for e in empresas %}
                 <tr>
-                    <td><a href="#">{{e.NomeEmpresa}}</a></td>
+                    <td class="td_maior"><a href="#">{{e.NomeEmpresa}}</a></td>
+                    <td class="td_maior"><a href="#">{{e.URLEmpresa}}</a></td>
+                    <td class="td_menor">{{e.ServidorEntradaEmpresa}}</td>
+                    <td class="td_menor">{{e.PortaServidorEntradaEmpresa}}</td>
+                    <td class="td_menor">{{e.ServidorSaidaEmpresa}}</td>
+                    <td class="td_menor">{{e.PortaServidorSaidaEmpresa}}</td>
                 </tr>
             {% endfor %}
 
         </tbody>
     </table>
+</div>
 
 {% include '/partials/Footer.php' %}
--- application/views/partials/Header.php
+++ application/views/partials/Header.php
@@ -3,34 +3,43 @@
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<title>{{ title ? title ~ separator }}Gerenciador de Dados</title>
-		<meta name="viewport" content="width=device-width">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title>{{ title ? title ~ separator }}Gerenciador de Dados</title>
+	<meta name="viewport" content="width=device-width">
 
-		{% for css in files.header.css %}
-			<link rel="stylesheet" href="/public/css/{{ css }}">
-		{% endfor %}
+	{% for css in files.header.css %}
+		<link rel="stylesheet" href="/public/css/{{ css }}">
+	{% endfor %}
 
-		{% for js in files.header.js %}
-			<script src="/public/js/{{ js }}"></script>
-		{% endfor %}
-	</head>
+	{% for js in files.header.js %}
+		<script src="/public/js/{{ js }}"></script>
+	{% endfor %}
+</head>
 
-	<body class="wrap">
-		<!-- Menu de navegação no topo da página -->
-<div class="navbar">
-  <div class="navbar-inner">
-    <a class="brand" href="#">Title</a>
-    <ul class="nav">
-    	<li class="active divider-vertical"><a href="#">Empresas</a></li>
-      	<li class="divider-vertical"><a href="#">Vendors</a></li>
-    </ul>
+<body>
+	<!-- Menu de navegação no topo da página -->
+	<div class="navbar navbar-inverse">
+	  <div class="navbar-inner">
+	    <div class="container wrap">
+	 
+	      <a class="brand" href="#">Gerenciador de Dados</a>
+		    <ul class="nav">
+		    	<li class="active divider-vertical"><a href="#">Empresas</a></li>
+		      	<li class="divider-vertical"><a href="#">Vendors</a></li>
+		    </ul>
 
-    <form class="navbar-search pull-left">
-	  	<input type="text" class="search-query" placeholder="Search">
-	</form>
-  </div>
-</div>
-
+		    <form class="navbar-search pull-left">
+			  	<input type="text" class="search-query" placeholder="Pequisar">
+			</form>
+	 
+	 
+	      <!-- Everything you want hidden at 940px or less, place within here -->
+	      <div class="nav-collapse collapse">
+	        <!-- .nav, .navbar-search, .navbar-form, etc -->
+	      </div>
+	 
+	    </div>
+	  </div>
+	</div>
